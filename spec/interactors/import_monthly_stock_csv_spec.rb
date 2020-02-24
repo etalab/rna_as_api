97ecd9997ccaa1c3b0817b8498a1d59def244734
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2020-02-24 13:15:24 +0200

    Import handle multiple files

--- spec/interactors/import_monthly_stock_csv_spec.rb
+++ spec/interactors/import_monthly_stock_csv_spec.rb
@@ -5,31 +5,25 @@ describe ImportMonthlyStockCsv do
   include_context 'mute interactors'
   include_context 'mute progress bar'
 
-  before(:all) do
-    Timecop.freeze(Time.utc(2018, 9, 1, 10, 5, 0))
-  end
+  subject { described_class.call(current_import: import_type, csv_filenames: filenames) }
 
-  after(:all) do
-    Timecop.return
-  end
+  before { Timecop.freeze(Time.utc(2018, 9, 1, 10, 5, 0)) }
 
-  context 'when importing waldec file' do
-    it 'succeed' do
-      expected_query_string = File.read('spec/fixtures/sample_files/query_string_rna_sample_waldec.txt')
-      sample_file = 'spec/fixtures/sample_files/rna_sample_waldec.csv'
+  context 'waldec files' do
+    let(:import_type) { 'waldec' }
+    let(:filenames) { ['spec/fixtures/sample_files/rna_sample_waldec.csv'] }
 
-      expect(ActiveRecord::Base).to receive_message_chain(:connection, :execute).with(expected_query_string)
-      described_class.call(current_import: 'waldec', csv_filename: sample_file)
+    it 'persists many associations' do
+      expect { subject }.to change(Association, :count).by 3
     end
   end
 
-  context 'when importing import file' do
-    it 'succeed' do
-      expected_query_string = File.read('spec/fixtures/sample_files/query_string_rna_sample_import.txt')
-      sample_file = 'spec/fixtures/sample_files/rna_sample_import.csv'
+  context 'rna files' do
+    let(:import_type) { 'import' }
+    let(:filenames) { ['spec/fixtures/sample_files/rna_sample_import.csv'] }
 
-      expect(ActiveRecord::Base).to receive_message_chain(:connection, :execute).with(expected_query_string)
-      described_class.call(current_import: 'import', csv_filename: sample_file)
+    it 'persists many associations' do
+      expect { subject }.to change(Association, :count).by 3
     end
   end
 end
